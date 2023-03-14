3ffa5be84cf5e3cbb271c0a6bc83b8b7fde4c155
root <root@DESKTOP-5QNGELR.localdomain>
2023-03-14 10:08:17 +0900

    MUIで少しUI改善

--- match/src/MyPage.tsx
+++ match/src/MyPage.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import { Button } from "@mui/material";
 
 export const MyPage = () => {
     return (
         <main>
             <h1>Hello World</h1>
             <Link to="/">Homeに戻る</Link>
+            <Button variant="contained" href="/setting1">
+                設定する
+            </Button>
         </main>
     )
 }
--- match/src/Road1.tsx
+++ match/src/Road1.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Loader from 'react-loader-spinner'
 
 export const Road1 = () => {
     return (
@@ -8,3 +9,34 @@ export const Road1 = () => {
         </main>
     )
 }
+
+// function Spinner() {
+//     return (
+//       <div className="sk-plane sk-center"></div>
+//     )
+// }
+
+// class App extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         loading: false
+//       }
+//     }
+//     componentDidMount() {
+//         // あえてローディング（スピナー）を表示するため5秒待機
+//         // 5秒後loadingのStateをTrueに変更
+//         setTimeout(()=>this.setState({loading: true}), 5000);
+//     }
+//       render () {
+//         return (
+//           <div>
+//             {this.state.loading ? (<Road1/>) : (<Spinner/>) }
+//           </div>
+//         )
+//     }
+// }
+// ReactDOM.render(
+//     <App/>,
+//     document.getElementById('root')
+// );
